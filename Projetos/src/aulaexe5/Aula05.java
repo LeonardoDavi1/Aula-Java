c27e72f7c6835123cfb6bbcbc50fa3d8b0d0c254
15375189682 <15375189682@BP-LABINF04.fha.local>
2023-08-15 18:47:18 -0300

    Meu primeiro commit

---
+++ Projetos/src/aulaexe5/Aula05.java
@@ -0,0 +1,24 @@
+package aulaexe5;
+
+/**
+ * Exercicio 5
+ * Disciplina: Linguagem de Programação
+ * Classe Math: constantes, principais métodos e chamando métodos de outras classes
+ * Aula: 15/08/2023
+ * @author Leonardo Davi Chagas Barbosa
+ */
+public class Aula05 {
+   public static void main(String[] args) {
+      System.out.println("O valor de pi e: "+ Math.PI);
+        System.out.println("Maximo: "+ Math.max(5, 10));
+          System.out.println("Minimo: "+ Math.min(5, 10));
+            System.out.println("Raiz Quadrada:"+ Math.sqrt(64));            
+            
+            System.out.println("Exponencial e potenciacao \n");
+          System.out.println("O valor de E e: " + Math.E + "\n\n");
+        System.out.println("'e' elevado ao quadrado = "+ Math.exp(2));
+      System.out.println("2 elevado" + Math.pow(2, 3)+ "\n\n");
+   
+   } 
+   
+}
