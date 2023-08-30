49f3e81c5c2aa0fef7c12f7a48b3be8f4ad8b377
Leonardo Davi <141193405+LeonardoDavi1@users.noreply.github.com>
2023-08-29 21:50:44 -0300

    Add files via upload

---
+++ aulaexe16/Usuario.java
@@ -0,0 +1,30 @@
+package aulaexe16;
+
+/**
+ * Aula:16
+ * Data: 29/08/2023
+ * @author Leonardo Davi Chagas Barbosa
+ */
+public class Usuario 
+{
+  private String nome = "Adm";  
+  private String senha = "T369";  
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
+ 
