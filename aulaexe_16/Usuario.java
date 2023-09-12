869bdcbd644fbedf1cdc397c79e3a51bbe7e9e4a
Leonardo Davi <141193405+LeonardoDavi1@users.noreply.github.com>
2023-09-12 19:37:24 -0300

    Add files via upload

---
+++ aulaexe_16/Usuario.java
@@ -0,0 +1,31 @@
+package aulaexe_16;
+
+/**
+ * 
+ */
+public class Usuario 
+{
+   private String usuario = "Adm";
+   private String senha = "T369"; 
+    
+   public String getUsuario()
+{ 
+    return usuario;
+}    
+    
+   public void setUsuario(String usuario)
+{    
+    this.usuario = usuario;    
+}
+   public String getSenha()
+{           
+        return senha;
+}
+   public void setSenha(String senha)
+{           
+    this.senha = senha;
+}
+}
+
+
+
